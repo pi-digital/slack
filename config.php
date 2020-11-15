59ee538a0b5a7705ba1d87d30e0442d7119adad9
pi-digital <54288024+pi-digital@users.noreply.github.com>
2020-11-15 19:07:31 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $timezone = 'America/Sao_Paulo';
 
-$slackApiToken = 'xoxb-1514572271025-1508949776052-DNnS0tocaFbq8XdNlDnbRByh';
+$slackApiToken = 'xoxb-1514572271025-1508949776052-Ms5PFLqx7dEEfYqDOwwHQEMg';
 $slackChannelName = 'agenda-de-autoridades';
 
 $slackHistoryCount = '25';
